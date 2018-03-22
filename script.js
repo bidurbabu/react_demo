d88d340ce16ccbe261bba91cb81e0dcefee7e363
Bidur Adhikari <badhikari@godaddy.com>
2018-03-22 14:04:56 -0700

    Adding react demo files

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log('hello world');
+
+$(document).ready(function() {
+  const jsContainer = document.getElementById("js");
+  const reactContainer = document.getElementById("react");
+  const render = () => {
+    jsContainer.innerHTML = `
+    <div class="demo">
+      Hello JS
+      <input />
+      <p>${new Date()}</p>
+    </div>
+  `;
+    ReactDOM.render(
+      React.createElement(
+        "div", {
+          className: "demo"
+        },
+        "Hello React ",
+        React.createElement("input"),
+        React.createElement(
+          "p",
+          null,
+          new Date().toString()
+        )
+      ),
+      reactContainer
+    );
+  }
+  setInterval(render, 1000);
+
+});
